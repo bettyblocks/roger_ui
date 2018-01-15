5ccde4e79758a1815ee5b4939e1aab2f70fa9886
Antonio Abella <antonioabella@designiostecnologicos.com>
2018-01-15 16:27:01 -0400

    feat: Allow configuration of web_port and run_server

--- lib/roger_ui/application.ex
+++ lib/roger_ui/application.ex
@@ -6,11 +6,15 @@ defmodule RogerUi.Application do
   use Application
 
   def start(_type, _args) do
-    case Plug.Adapters.Cowboy.http(RogerUi.RouterPlug, [], port: 4040) do
-      {:ok, _} ->
-        IO.puts("Starting RogerUi server on port 4040")
-      {:error, :eaddrinuse} ->
-        IO.puts("RogerUi Server already have been started on port 4040")
+    run_server? = Application.get_env(:roger_ui, :server, true)
+    web_port = Application.get_env(:roger_ui, :web_port, 4040)
+    if run_server? do
+      case Plug.Adapters.Cowboy.http(RogerUi.RouterPlug, [], port: web_port) do
+        {:ok, _} ->
+          IO.puts("Starting RogerUi server on port #{web_port}")
+        {:error, :eaddrinuse} ->
+          IO.puts("RogerUi Server already have been started on port #{web_port}")
+      end
     end
     Supervisor.start_link([], strategy: :one_for_one)
   end
