75c32b9e6a317e2b0fa5feca80e68beddb3a9e36
Antonio Abella <antonioabella@designiostecnologicos.com>
2018-01-30 07:52:31 -0400

    feat(api): add access-control-allow-origin to enable CORS call

--- priv/vue/config/dev.env.js
+++ priv/vue/config/dev.env.js
@@ -3,5 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  // Change the API base url.
+  BASE_URL: '"http://192.168.1.175:4040/"',
 })
