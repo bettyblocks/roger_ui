ec8f38d61fa3595258b5f4fd9ffa201120ff3b63
Antonio Abella <antonioabella@designiostecnologicos.com>
2018-02-02 06:37:27 -0400

    feat: checkbox list and action buttons

--- priv/vue/src/components/Queues.vue
+++ priv/vue/src/components/Queues.vue
@@ -1,15 +1,32 @@
 <template>
 <div class="container">
-  <b-row>
-    <b-col cols="2" class="my-1">
+  <b-row class="my-1">
+    <b-col cols="2">
       <b-pagination @change="change_page"
-        size="sm" :total-rows="total_queues" :per-page="page_size"></b-pagination>
+                    size="sm" :total-rows="total_queues"
+                    :per-page="page_size">
+      </b-pagination>
     </b-col>
-    <b-col md="10" class="my-1">
+    <b-col cols="8">
       <b-form-input @input="change_filter" placeholder="Type to Filter" autofocus />
     </b-col>
+    <b-col cols="2">
+      <b-button-toolbar class="my-1">
+        <b-button-group size="sm">
+          <b-btn class="mx-1 mdi mdi-play"></b-btn>
+          <b-btn class="mdi mdi-pause"></b-btn>
+          <b-btn class="mx-1 mdi mdi-cancel"></b-btn>
+        </b-button-group>
+      </b-button-toolbar>
+    </b-col>
   </b-row>
-  <b-table small :items="queues" :fields="fields"></b-table>
+  <b-table small :items="queues" :fields="fields">
+    <template slot="HEAD_actions" slot-scope="head">
+      {{head.label}} &nbsp;
+      <input type="checkbox" @click.stop="toggleSelected" v-model="allSelected">
+    </template>
+    <template slot="actions" slot-scope="item"><input type="checkbox"></template>
+  </b-table>
 </div>
 </template>
 
@@ -24,11 +41,16 @@ export default {
         qualified_queue_name: {
           label: 'Name'
         },
-        count: {
-          label: 'Count'
-        },
         paused: {
           label: 'Status'
+        },
+        count: {
+          label: 'Count',
+          'class': 'text-right'
+        },
+        actions: {
+          label: 'All',
+          'class': 'text-right'
         }
       },
       queues: [],
