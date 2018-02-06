37d312b4735fc3cf0a62eae882f8cac1329c471c
Antonio Abella <antonioabella@designiostecnologicos.com>
2018-02-06 15:37:52 -0400

    refactor: use delegate to call Roger API

--- lib/roger_ui/roger_api.ex
+++ lib/roger_ui/roger_api.ex
@@ -5,22 +5,14 @@ defmodule RogerUi.RogerApi do
 
   @behaviour RogerUi.Roger
 
-  def partitions(), do: Roger.Info.partitions()
+  defdelegate purge_queue(partition_name, queue_name), to: Roger.Queue, as: :purge
 
-  def purge_queue(partition_name, queue_name), do:
-    Roger.Queue.purge(partition_name, queue_name)
+  defdelegate queue_pause(partition_name, queue_name), to: Roger.Partition.Global
+  defdelegate queue_resume(partition_name, queue_name), to: Roger.Partition.Global
+  defdelegate cancel_job(partition_name, queue_name), to: Roger.Partition.Global
 
-  def queue_pause(partition_name, queue_name), do:
-    Roger.Partition.Global.queue_pause(partition_name, queue_name)
-
-  def queue_resume(partition_name, queue_name), do:
-    Roger.Partition.Global.queue_resume(partition_name, queue_name)
-
-  def cancel_job(partition_name, job_name), do:
-    Roger.Partition.Global.cancel_job(partition_name, job_name)
-
-  def running_jobs(partition_name), do: Roger.Info.running_jobs(partition_name)
-
-  def queued_jobs(partition_name, queue_name, count \\ 100), do:
-      Roger.Info.queued_jobs(partition_name, queue_name, count)
+  defdelegate partitions, to: Roger.Info
+  defdelegate running_jobs(), to: Roger.Info
+  defdelegate running_jobs(partition_name), to: Roger.Info
+  defdelegate queued_jobs(partition_name, queue_name, count \\ 100), to: Roger.Info
 end
