40350b0dc65c507c09c719d3b6bb98e0834b5023
Antonio Abella <antonioabella@designiostecnologicos.com>
2018-02-28 20:31:50 -0400

    feat: RogerUi.Partitions

---
+++ lib/roger_ui/partitions.ex
@@ -0,0 +1,25 @@
+defmodule RogerUi.Partitions do
+  @moduledoc """
+  Generate Partition list from Roger.Info.partitions()
+  """
+
+  def normalize(partitions) do
+    partitions
+    |> Enum.flat_map(&capture_node_name(&1))
+  end
+
+  defp capture_node_name({node_name, partitions}) do
+    partitions
+    |> Enum.flat_map(&normalize_partition(node_name, &1))
+  end
+
+  defp normalize_partition(node_name, {status, partitions}) do
+    Enum.map(partitions, fn {partition_name, _} ->
+        %{
+          "node_name" => node_name,
+          "status" => status,
+          "partition_name" => partition_name
+        }
+    end)
+  end
+end
