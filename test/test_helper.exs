410faafacb8e0b4cccb96ec64bffe681373980b7
ingsissam <33227372+ingsissam@users.noreply.github.com>
2018-03-02 15:51:02 -0400

    refactor: Renaming the test modules
    To work with the new structure

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,6 @@
 [:cowboy, :plug, :mox]
 |> Enum.each(&Application.ensure_all_started/1)
 
-Mox.defmock(RogerUi.RogerApi.Mock, for: RogerUi.Roger)
+Mox.defmock(RogerUI.RogerApi.Mock, for: RogerUI.Roger)
 
 ExUnit.start(exclude: [:slow])
