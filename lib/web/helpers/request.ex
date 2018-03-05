07da8dba69bc3a696c0307f07dd28329f9bc8a23
Spadavecchia <antonioabella@designiostecnologicos.com>
2018-03-05 18:42:45 -0400

    Update request.ex

--- lib/web/helpers/request.ex
+++ lib/web/helpers/request.ex
@@ -1,9 +1,8 @@
 defmodule RogerUI.Web.Helpers.Request do
   @moduledoc """
-  This module provides Roger UI helper functions. These functions are used in order to prepare request methods of:
+  Prepare request methods of:
   - parameters reading.
   - JSON parsing.
-  Request Helper functions may be required by any module used by Roger UI and these are independents of business logic.
   """
 
   alias Plug.Parsers
