1b9bf042f5d81d667f391e3778b1574153b80b29
Spadavecchia <antonioabella@designiostecnologicos.com>
2018-03-05 18:46:27 -0400

    Update jobs_plug_test.exs

--- test/web/jobs_plug_test.exs
+++ test/web/jobs_plug_test.exs
@@ -1,9 +1,4 @@
 defmodule RogerUI.Web.JobsPlugTest do
-  @moduledoc """
-  This module provides Roger UI test functions. It tests the behaviour
-  of RogerUi.Web.JobsPlug, RogerUi.RogerApi functions. This module includes
-  RogerUi.Tests.RogerApiInMemory functions, necessary to simulate Roger API responses.
-  """
   use ExUnit.Case, async: true
   use Plug.Test
   alias RogerUI.Web.JobsPlug.Router
