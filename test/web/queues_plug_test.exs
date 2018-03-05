9197a60b047a2658950f7a5658284759fa0ed58e
Spadavecchia <antonioabella@designiostecnologicos.com>
2018-03-05 18:46:52 -0400

    Update queues_plug_test.exs

--- test/web/queues_plug_test.exs
+++ test/web/queues_plug_test.exs
@@ -1,9 +1,4 @@
 defmodule RogerUI.Web.QueuesPlugTest do
-  @moduledoc """
-  This module provides Roger UI test functions. It tests the behaviour
-  of RogerUi.Web.QueuesPlug.Router functions. This module includes
-  RogerUi.Tests.RogerApiInMemory functions, necessary to simulate Roger API responses.
-  """
   use ExUnit.Case, async: true
   use Plug.Test
   alias RogerUI.Web.QueuesPlug.Router
