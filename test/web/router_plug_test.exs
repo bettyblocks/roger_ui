e65708fc3489ca0dab15d5a21dc9a8f04a5109b7
Spadavecchia <antonioabella@designiostecnologicos.com>
2018-03-05 18:47:17 -0400

    Update router_plug_test.exs

--- test/web/router_plug_test.exs
+++ test/web/router_plug_test.exs
@@ -1,10 +1,4 @@
 defmodule RogerUI.Web.RouterPlugTest do
-  @moduledoc """
-  This module provides Roger UI test functions. It tests the behaviour
-  of RogerUi.Web.RouterPlug functions. This module includes
-  RogerUi.Tests.RogerApiInMemory functions, necessary to simulate Roger API responses.
-  """
-defmodule RogerUI.Web.RouterPlugTest do
   use ExUnit.Case
   use Plug.Test
   alias RogerUI.Web.RouterPlug.Router
