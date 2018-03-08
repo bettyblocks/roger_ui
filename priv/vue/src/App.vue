4f944f0b61abb7b2139f1663fdb158b52eadb7d4
Antonio Abella <antonioabella@designiostecnologicos.com>
2018-03-07 22:38:46 -0400

    feat: v-bottom-nav instead of v-navigation-drawer

--- priv/vue/src/App.vue
+++ priv/vue/src/App.vue
@@ -1,60 +1,30 @@
 <template>
   <v-app id="RogerUI">
-    <v-navigation-drawer
-      fixed
-      :clipped="$vuetify.breakpoint.lgAndUp"
-      v-model="drawer"
-      app
-    >
-      <v-list dense>
-        <v-list-tile :to="{path: '/'}">
-          <v-list-tile-action>
-            <v-icon>dashboard</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Dashboard</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile :to="{path: '/partitions'}">
-          <v-list-tile-action>
-            <v-icon>line_style</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Partitions</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile :to="{path: '/queues'}">
-          <v-list-tile-action>
-            <v-icon>lists</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Queues</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile :to="{path: '/jobs'}">
-          <v-list-tile-action>
-            <v-icon>restore</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Jobs</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      color="blue darken-3"
-      app
-      :clipped-left="$vuetify.breakpoint.lgAndUp"
-      fixed
-    >
-      <v-toolbar-title>
-        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <span class="hidden-sm-and-down">RogerUI</span>
-      </v-toolbar-title>
-    </v-toolbar>
     <v-content>
       <router-view />
     </v-content>
+    <v-bottom-nav
+      app
+      absolute
+      :value="true">
+      <v-btn :to="{path: '/'}" flat color="teal">
+        <span>Dashboard</span>
+        <v-icon>dashboard</v-icon>
+      </v-btn>
+      <v-btn :to="{path: '/partitions'}" flat color="teal">
+        <span>Partitions</span>
+        <v-icon>line_style</v-icon>
+      </v-btn>
+      <v-btn :to="{path: '/queues'}" flat color="teal">
+        <span>Queues</span>
+        <v-icon>lists</v-icon>
+      </v-btn>
+      <v-btn :to="{path: '/jobs'}" flat color="teal">
+        <span>Jobs</span>
+        <v-icon>restore</v-icon>
+      </v-btn>
+      <span class="title mt-3">RogerUI</span>
+    </v-bottom-nav>
   </v-app>
 </template>
 
