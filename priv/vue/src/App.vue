a58ae5d9fd12cfa5ae77f43e41a7a9010b05ea49
Antonio Abella <antonioabella@designiostecnologicos.com>
2018-03-08 21:06:32 -0400

    feat: toolbar instead of bottom-nav

--- priv/vue/src/App.vue
+++ priv/vue/src/App.vue
@@ -1,30 +1,29 @@
 <template>
   <v-app id="RogerUI">
+    <v-toolbar
+      app
+      dense
+    >
+      <v-toolbar-title>RogerUI</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn :to="{path: '/'}" flat>
+          <span>Dashboard</span>
+        </v-btn>
+        <v-btn :to="{path: '/partitions'}" flat>
+          <span>Partitions</span>
+        </v-btn>
+        <v-btn :to="{path: '/queues'}" flat>
+          <span>Queues</span>
+        </v-btn>
+        <v-btn :to="{path: '/jobs'}" flat>
+          <span>Jobs</span>
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
     <v-content>
       <router-view />
     </v-content>
-    <v-bottom-nav
-      app
-      absolute
-      :value="true">
-      <v-btn :to="{path: '/'}" flat color="teal">
-        <span>Dashboard</span>
-        <v-icon>dashboard</v-icon>
-      </v-btn>
-      <v-btn :to="{path: '/partitions'}" flat color="teal">
-        <span>Partitions</span>
-        <v-icon>line_style</v-icon>
-      </v-btn>
-      <v-btn :to="{path: '/queues'}" flat color="teal">
-        <span>Queues</span>
-        <v-icon>lists</v-icon>
-      </v-btn>
-      <v-btn :to="{path: '/jobs'}" flat color="teal">
-        <span>Jobs</span>
-        <v-icon>restore</v-icon>
-      </v-btn>
-      <span class="title mt-3">RogerUI</span>
-    </v-bottom-nav>
   </v-app>
 </template>
 
